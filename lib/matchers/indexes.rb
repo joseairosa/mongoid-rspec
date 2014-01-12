9ef0509392f0b40c59c55e1464a91dca31559e26
José P. Airosa <me@joseairosa.com>
2014-01-12 12:12:30 +0000

    Fixing index matcher for mongoid >= 4

--- lib/matchers/indexes.rb
+++ lib/matchers/indexes.rb
@@ -14,13 +14,19 @@ module Mongoid
         @klass  = klass.is_a?(Class) ? klass : klass.class
         @errors = []
 
-        unless @klass.index_options[@index_fields]
+        if Mongoid::VERSION.to_i < 4
+          index_options = @klass.index_options
+        else
+          index_options = Hash[@klass.index_specifications.map{|i| [i.key, i.options]}]
+        end
+
+        unless index_options[@index_fields]
           @errors.push "no index for #{@index_fields}"
         else
           if !@options.nil? && !@options.empty?
             @options.each do |option, option_value|
-              if @klass.index_options[@index_fields][option] != option_value
-                @errors.push "index for #{@index_fields.inspect} with options of #{@klass.index_options[@index_fields].inspect}"
+              if index_options[@index_fields][option] != option_value
+                @errors.push "index for #{@index_fields.inspect} with options of #{index_options[@index_fields].inspect}"
               end
             end
           end
